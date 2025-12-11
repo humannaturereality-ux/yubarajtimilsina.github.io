ce51c0daa00487de7666ee8f5a8efee6dd6abcc6
humannaturereality-ux <human.nature.reality@gmail.com>
2025-12-11 13:29:15 +0545

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base:"yubarajtimilsina.github.io",
+  base:"/yubarajtimilsina.github.io/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
